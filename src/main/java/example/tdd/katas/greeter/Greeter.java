af2d962c0a312312a926fcc70eae5953e3c4053a
AlexParker6 <alex.parker6@nhs.net>
2020-10-15 14:39:23 +0100

    Test 1 - should greet with name, test passed Test 2 - should trim name, test passes Test 3 should capitalise name, test passes

--- src/main/java/example/tdd/katas/greeter/Greeter.java
+++ src/main/java/example/tdd/katas/greeter/Greeter.java
@@ -3,7 +3,6 @@ package example.tdd.katas.greeter;
 public class Greeter {
 
     String greet(String name) {
-        String greet = name;
         name = name.trim();
         return "Hello " + name.substring(0,1).toUpperCase() + name.substring(1);
     }
