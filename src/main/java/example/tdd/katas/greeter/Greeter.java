1226c6ff0335c79d0e4b0e5852b1af486652ea8d
AlexParker6 <alex.parker6@nhs.net>
2020-10-15 15:17:42 +0100

    All tests pass

--- src/main/java/example/tdd/katas/greeter/Greeter.java
+++ src/main/java/example/tdd/katas/greeter/Greeter.java
@@ -4,7 +4,18 @@ public class Greeter {
 
     String greet(String name) {
         name = name.trim();
-        return "Hello " + name.substring(0,1).toUpperCase() + name.substring(1);
+        name = name.substring(0,1).toUpperCase() + name.substring(1);
+
+        int timeOfDay = 14;
+
+        if (timeOfDay >=6 && timeOfDay <=12){
+            return "Good morning " + name; }
+        else if (timeOfDay >=18 && timeOfDay <=22){
+            return "Good evening " + name; }
+        else if (timeOfDay >22 || timeOfDay <=5.59){
+            return "Good night " + name; }
+        else
+            return "Hello " + name;
     }
 
 }
--- src/test/java/example/tdd/katas/greeter/GreeterTest.java
+++ src/test/java/example/tdd/katas/greeter/GreeterTest.java
@@ -23,7 +23,31 @@ public class GreeterTest {
     @Test
     public void shouldCapitaliseName() {
         Greeter greeter = new Greeter();
-        assertEquals("Hello Bob", greeter.greet("bob"));
-        assertEquals("Hello Bill", greeter.greet("bill"));
+        assertEquals("Hello Bob", greeter.greet(" bob"));
+        assertEquals("Hello Bill", greeter.greet(" bill"));
+    }
+
+    @Test
+    public void shouldGreetGoodMorning() {
+        Greeter greeter = new Greeter();
+        assertEquals("Good morning Bob", greeter.greet(" bob"));
+        assertEquals("Good morning Bill", greeter.greet(" bill"));
+
+    }
+
+    @Test
+    public void shouldGreetGoodEvening() {
+        Greeter greeter = new Greeter();
+        assertEquals("Good evening Bob", greeter.greet(" bob"));
+        assertEquals("Good evening Bill", greeter.greet(" bill"));
+
+    }
+
+    @Test
+    public void shouldGreetGoodnight() {
+        Greeter greeter = new Greeter();
+        assertEquals("Good night Bob", greeter.greet(" bob"));
+        assertEquals("Good night Bill", greeter.greet(" bill"));
+
     }
 }
